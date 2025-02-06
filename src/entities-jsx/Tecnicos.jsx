572f3e6e83fa7de7b30d8facc0e4843cd94e3173
Federico Bremberg <fedebremberg@gmail.com>
2025-02-06 20:07:36 -0300

    modifico tecnicos
    modifico los accesos a los forms

--- src/entities-jsx/Tecnicos.jsx
+++ src/entities-jsx/Tecnicos.jsx
@@ -3,7 +3,7 @@ import Sidebar from "./Sidebar";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap/dist/js/bootstrap.bundle.min.js";
 
-function Tecnicos() {
+function Tecnicos({ user }) {
   return (
     <div className="Tecnicos d-flex">
       <Sidebar />
@@ -12,57 +12,64 @@ function Tecnicos() {
         <h2 className="pb-2 border-bottom">Técnicos de Heladeras</h2>
 
         <div className="row g-4 py-5 row-cols-1 row-cols-md-2 row-cols-lg-4">
-          {/* Registrar Incidente */}
-          <div className="col">
-            <div className="card shadow-sm p-3 text-center">
-              <h3 className="fs-4">Registrar Incidente</h3>
-              <p>
-                Registra las visitas de los incidentes en los que trabajaste
-              </p>
-              <a
-                href="/tecnicos/registrar-incidente"
-                className="btn btn-primary"
-              >
-                Registrar
-              </a>
+          {/* Registrar Incidente (Visible para ADMIN y TECNICO) */}
+          {(user?.rol === "ADMIN" || user?.rol === "TECNICO") && (
+            <div className="col">
+              <div className="card shadow-sm p-3 text-center">
+                <h3 className="fs-4">Registrar Incidente</h3>
+                <p>
+                  Registra las visitas de los incidentes en los que trabajaste
+                </p>
+                <a
+                  href="/tecnicos/registrar-incidente"
+                  className="btn btn-primary"
+                >
+                  Registrar
+                </a>
+              </div>
             </div>
-          </div>
+          )}
 
-          {/* Nuevo Técnico */}
-          <div className="col">
-            <div className="card shadow-sm p-3 text-center">
-              <h3 className="fs-4">Nuevo Técnico</h3>
-              <p>Dar de alta a un nuevo técnico en el sistema</p>
-              <a href="/tecnicos/tecnicos-alta" className="btn btn-primary">
-                Alta
-              </a>
-            </div>
-          </div>
+          {/* Solo visible para administradores */}
+          {user?.rol === "ADMIN" && (
+            <>
+              {/* Nuevo Técnico */}
+              <div className="col">
+                <div className="card shadow-sm p-3 text-center">
+                  <h3 className="fs-4">Nuevo Técnico</h3>
+                  <p>Dar de alta a un nuevo técnico en el sistema</p>
+                  <a href="/tecnicos/tecnicos-alta" className="btn btn-primary">
+                    Alta
+                  </a>
+                </div>
+              </div>
 
-          {/* Modificar Técnico */}
-          <div className="col">
-            <div className="card shadow-sm p-3 text-center">
-              <h3 className="fs-4">Modificar Técnico</h3>
-              <p>Modificar a un técnico ya existente en el sistema</p>
-              <a
-                href="/tecnicos/tecnicos-modificacion"
-                className="btn btn-primary"
-              >
-                Modificar
-              </a>
-            </div>
-          </div>
+              {/* Modificar Técnico */}
+              <div className="col">
+                <div className="card shadow-sm p-3 text-center">
+                  <h3 className="fs-4">Modificar Técnico</h3>
+                  <p>Modificar a un técnico ya existente en el sistema</p>
+                  <a
+                    href="/tecnicos/tecnicos-modificacion"
+                    className="btn btn-primary"
+                  >
+                    Modificar
+                  </a>
+                </div>
+              </div>
 
-          {/* Dar de Baja Técnico */}
-          <div className="col">
-            <div className="card shadow-sm p-3 text-center">
-              <h3 className="fs-4">Dar de Baja un Técnico</h3>
-              <p>Eliminar un técnico en el sistema</p>
-              <a href="/tecnicos/tecnicos-baja" className="btn btn-primary">
-                Dar de Baja
-              </a>
-            </div>
-          </div>
+              {/* Dar de Baja Técnico */}
+              <div className="col">
+                <div className="card shadow-sm p-3 text-center">
+                  <h3 className="fs-4">Dar de Baja un Técnico</h3>
+                  <p>Eliminar un técnico en el sistema</p>
+                  <a href="/tecnicos/tecnicos-baja" className="btn btn-primary">
+                    Dar de Baja
+                  </a>
+                </div>
+              </div>
+            </>
+          )}
         </div>
       </div>
     </div>
